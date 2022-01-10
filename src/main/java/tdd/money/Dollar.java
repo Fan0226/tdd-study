143dd434ac90e7600101224aa05b74a6767c84a6
Fan0226 <kiku0226@icloud.com>
2022-01-10 10:49:22 +0900

    Chapter 8.

--- src/main/java/tdd/money/Dollar.java
+++ src/main/java/tdd/money/Dollar.java
@@ -1,15 +1,11 @@
-// package tdd.example;
 package tdd.money;
 
-import java.util.function.DoubleSupplier;
-
 class Dollar extends Money {
-
     Dollar(int amount) {
         this.amount = amount;
     }
 
-    Dollar times(int multiplier) {
+    Money times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
 }
