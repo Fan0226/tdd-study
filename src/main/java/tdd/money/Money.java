f44edb288097644fa0c43ca75e3a1f54ce573f75
Fan0226 <kiku0226@icloud.com>
2022-04-10 12:58:07 +0900

    Chapter 14.

--- src/main/java/tdd/money/Money.java
+++ src/main/java/tdd/money/Money.java
@@ -19,8 +19,11 @@ class Money implements Expression {
         return new Sum(this, addend);
     }
 
-    public Money reduce(String to) {
-        return this;
+    public Money reduce(Bank bank, String to) {
+        // return this;
+        // int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        int rate = bank.rate(currency, to);
+        return new Money(amount / rate, to);
     }
 
     String currency() {
